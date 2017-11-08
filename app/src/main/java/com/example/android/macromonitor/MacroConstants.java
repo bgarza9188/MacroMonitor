101df52e3b54d7ef6f28c572ced174606da8929b
bgarza9188 <bgarza9188@gmail.com>
2017-11-08 15:26:11 -0600

    Create and Update Data is roughly complete.

--- app/src/main/java/com/example/android/macromonitor/MacroConstants.java
+++ app/src/main/java/com/example/android/macromonitor/MacroConstants.java
@@ -1,15 +1,20 @@
 package com.example.android.macromonitor;
 
+import com.example.android.macromonitor.data.MacroContract;
+
 /**
  * Created by jebus on 11/8/2017.
  */
 
 public interface MacroConstants {
-    String MACRO_SUGAR_DB_NAME = "Sugar";
-    String MACRO_FAT_DB_NAME = "Fat";
-    String MACRO_CARB_DB_NAME = "Carbohydrates";
-    String MACRO_WATER_DB_NAME = "Water";
-
+    String MACRO_SUGAR_DB_NAME = "SUGAR";
+    String MACRO_FAT_DB_NAME = "FAT";
+    String MACRO_CARB_DB_NAME = "CARBOHYDRATES";
+    String MACRO_WATER_DB_NAME = "WATER";
+    String LOADER_DB_NAME_ARG = MacroContract.MacroEntry.COLUMN_MACRO_NAME + " = '";
+    String LOADER_ARG_CLOSE_QUOTE = "'";
+    String LOADER_ARG_BUNDLE_KEY_SELECTION = "SELECTION";
+    String SORT_ORDER_LATEST_RECORD = MacroContract.MacroEntry.COLUMN_INTAKE_DATE + " DESC LIMIT 1";
 
 
 }
--- app/src/main/java/com/example/android/macromonitor/MainActivity.java
+++ app/src/main/java/com/example/android/macromonitor/MainActivity.java
@@ -11,17 +11,25 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.android.macromonitor.data.MacroContract;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, MacroConstants{
 
-    private static final int MACRO_LOADER_ONE = 0;
-    private static final int MACRO_LOADER_TWO = 1;
-    private static final int MACRO_LOADER_THREE = 2;
-    private static final int MACRO_LOADER_FOUR = 3;
-    private TextView textView;
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private static final int MACRO_LOADER_ONE = 1;
+    private static final int MACRO_LOADER_TWO = 2;
+    private static final int MACRO_LOADER_THREE = 3;
+    private static final int MACRO_LOADER_FOUR = 4;
+    private TextView textView_quadrant_one;
+    private TextView textView_quadrant_two;
+    private TextView textView_quadrant_three;
+    private TextView textView_quadrant_four;
+    private Button button_quadrant_one;
+    private Button button_quadrant_two;
+    private Button button_quadrant_three;
+    private Button button_quadrant_four;
 
     private static final String[] MACRO_COLUMNS = {
             MacroContract.MacroEntry.TABLE_NAME + "." + MacroContract.MacroEntry._ID,
@@ -41,19 +49,43 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.quardrant_one_text);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    ContentValues contentValues = new ContentValues();
-                    contentValues.put(MacroContract.MacroEntry.COLUMN_MACRO_NAME, MACRO_SUGAR_DB_NAME);
-                    contentValues.put(MacroContract.MacroEntry.COLUMN_INTAKE_VALUE, 1);
-                    contentValues.put(MacroContract.MacroEntry.COLUMN_INTAKE_DATE, "20171108");
-                    getContentResolver().insert(MacroContract.MacroEntry.CONTENT_URI,contentValues);
-            }
-        });
-        getSupportLoaderManager().initLoader(MACRO_LOADER_ONE, null, this);
+        textView_quadrant_one = findViewById(R.id.quadrant_one_text);
+        button_quadrant_one = findViewById(R.id.quadrant_one_button);
+
+        textView_quadrant_two = findViewById(R.id.quadrant_two_text);
+        button_quadrant_two = findViewById(R.id.quadrant_two_button);
+
+        textView_quadrant_three = findViewById(R.id.quadrant_three_text);
+        button_quadrant_three = findViewById(R.id.quadrant_three_button);
+
+        textView_quadrant_four = findViewById(R.id.quadrant_four_text);
+        button_quadrant_four = findViewById(R.id.quadrant_four_button);
+
+        //TODO need to call insert to insert new rows for each new day
+        //initDBInserts();
+        //This will init a loader that will pull all rows
+        //getSupportLoaderManager().initLoader(MACRO_LOADER_ONE, null, this);
+        Bundle bundle = new Bundle();
+        bundle.putString(LOADER_ARG_BUNDLE_KEY_SELECTION, LOADER_DB_NAME_ARG + MACRO_WATER_DB_NAME + LOADER_ARG_CLOSE_QUOTE);
+        getSupportLoaderManager().initLoader(MACRO_LOADER_ONE, bundle, this);
+
+        bundle.putString(LOADER_ARG_BUNDLE_KEY_SELECTION, LOADER_DB_NAME_ARG + MACRO_FAT_DB_NAME + LOADER_ARG_CLOSE_QUOTE);
+        getSupportLoaderManager().initLoader(MACRO_LOADER_TWO, bundle, this);
+
+        bundle.putString(LOADER_ARG_BUNDLE_KEY_SELECTION, LOADER_DB_NAME_ARG + MACRO_CARB_DB_NAME + LOADER_ARG_CLOSE_QUOTE);
+        getSupportLoaderManager().initLoader(MACRO_LOADER_THREE, bundle, this);
+
+        bundle.putString(LOADER_ARG_BUNDLE_KEY_SELECTION, LOADER_DB_NAME_ARG + MACRO_SUGAR_DB_NAME + LOADER_ARG_CLOSE_QUOTE);
+        getSupportLoaderManager().initLoader(MACRO_LOADER_FOUR, bundle, this);
+    }
+
+    private void initDBInserts() {
+//        ContentValues contentValues = new ContentValues();
+//
+//        contentValues.put(MacroContract.MacroEntry.COLUMN_MACRO_NAME, MACRO_WATER_DB_NAME);
+//        contentValues.put(MacroContract.MacroEntry.COLUMN_INTAKE_VALUE, 0);
+//        contentValues.put(MacroContract.MacroEntry.COLUMN_INTAKE_DATE, "20171108");
+//        getContentResolver().insert(MacroContract.MacroEntry.CONTENT_URI,contentValues);
     }
 
     @Override
@@ -63,66 +95,105 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        CursorLoader loader;
-        switch (id) {
-            case 0:  loader = new CursorLoader(this,
+        CursorLoader loader = new CursorLoader(this,
                     MacroContract.MacroEntry.CONTENT_URI,
                     MACRO_COLUMNS,
+                    args.getString(LOADER_ARG_BUNDLE_KEY_SELECTION),
                     null,
-                    null,
-                    null);
-                break;
-            case 1: loader = new CursorLoader(this,
-                    MacroContract.MacroEntry.CONTENT_URI,
-                    MACRO_COLUMNS,
-                    "macro_name=" + MACRO_FAT_DB_NAME,
-                    null,
-                    null);
-                break;
-            case 2: loader = new CursorLoader(this,
-                    MacroContract.MacroEntry.CONTENT_URI,
-                    MACRO_COLUMNS,
-                    "macro_name=" + MACRO_CARB_DB_NAME,
-                    null,
-                    null);
-                break;
-            case 3: loader = new CursorLoader(this,
-                    MacroContract.MacroEntry.CONTENT_URI,
-                    MACRO_COLUMNS,
-                    "macro_name=" + MACRO_SUGAR_DB_NAME,
-                    null,
-                    null);
-                break;
-            default: loader = null;
-                break;
-        }
+                    SORT_ORDER_LATEST_RECORD);
 
         return loader;
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        Log.e("MainActivity", "Ben in onLoadFinished");
+        Log.e(LOG_TAG, "Ben in onLoadFinished, loader ID:" + loader.getId());
         if(data == null || (data != null && data.getCount() == 0)){
-            Toast.makeText(this, "No results",
-                    Toast.LENGTH_SHORT).show();
+            Log.e(LOG_TAG, "Ben on results from loader");
         }else{
-            loader.getId();
-            Log.e("Ben", "data getCount:" + data.getCount());
-//            data.moveToFirst();
-//            String name = data.getString(COL_MACRO_NAME);
-//            int value = data.getInt(COL_INTAKE_VALUE);
-//            String date = data.getString(COL_INTAKE_DATE);
-//            textView.setText("Name:" + name + " Value:" + value + " date: " + date);
-            //Toasty.success(getApplicationContext(), "NAME:" + name + " VALUE:" + value + " DATE:" + date, Toast.LENGTH_LONG, true).show();
-//            String names[] = data.getColumnNames();
-//
-//            for(int i=0; i<names.length; i++){
-//                Log.e("Ben", "col_name:" + names[i]);
-//                Log.e("Ben", "col_index:" + data.getColumnIndex(names[i]));
-//            }
-            //Toasty.info(getApplicationContext(), "Column Count:" + data.getColumnCount(), Toast.LENGTH_LONG, true).show();
+            Log.e(LOG_TAG, "Ben data getCount:" + data.getCount());
+            data.moveToFirst();
+            do{
+                updateUI(loader.getId(), data);
+                Log.e(LOG_TAG, "Ben macro names:" + data.getString(COL_MACRO_NAME) + " record Date:" + data.getString(COL_INTAKE_DATE) + " record ID:" + data.getInt(COL_MACRO_ENTRY_ID) + " value:" + data.getInt(COL_INTAKE_VALUE));
+            }while(data.moveToNext());
+
         }
+    }
+
+    private void updateUI(int id, final Cursor data) {
+        data.moveToFirst();//sanity check
+        String name = data.getString(COL_MACRO_NAME);
+        final int value = data.getInt(COL_INTAKE_VALUE);
+        if(id == 1){
+            textView_quadrant_one.setText("Name:" + name + ", value:" + value);
+            button_quadrant_one.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Make an update call to DB
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(MacroContract.MacroEntry.COLUMN_MACRO_NAME, MACRO_WATER_DB_NAME);
+                    contentValues.put(MacroContract.MacroEntry.COLUMN_INTAKE_VALUE, value+1);
+                    //Ideally the date set here will be today's date
+                    contentValues.put(MacroContract.MacroEntry.COLUMN_INTAKE_DATE, "20171108");
+                    //The 'macro_name' in the where clause should match the quadrant and the update value
+                    //The date here should also ideally be today's date
+                    getContentResolver().update(MacroContract.MacroEntry.CONTENT_URI,contentValues, "macro_name = 'WATER' AND intake_date = '20171108'", null);
+                }
+            });
+        } else if(id == 2) {
+            textView_quadrant_two.setText("Name:" + name + ", value:" + value);
+            button_quadrant_two.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Make an update call to DB
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(MacroContract.MacroEntry.COLUMN_MACRO_NAME, MACRO_FAT_DB_NAME);
+                    //just doing '+1' for now
+                    contentValues.put(MacroContract.MacroEntry.COLUMN_INTAKE_VALUE, value+1);
+                    //Ideally the date set here will be today's date
+                    contentValues.put(MacroContract.MacroEntry.COLUMN_INTAKE_DATE, "20171108");
+                    //The 'macro_name' in the where clause should match the quadrant and the update value
+                    //The date here should also ideally be today's date
+                    getContentResolver().update(MacroContract.MacroEntry.CONTENT_URI,contentValues, "macro_name = 'FAT' AND intake_date = '20171108'", null);
+                }
+            });
+        } else if(id == 3) {
+            textView_quadrant_three.setText("Name:" + name + ", value:" + value);
+            button_quadrant_three.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Make an update call to DB
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(MacroContract.MacroEntry.COLUMN_MACRO_NAME, MACRO_CARB_DB_NAME);
+                    //just doing '+1' for now
+                    contentValues.put(MacroContract.MacroEntry.COLUMN_INTAKE_VALUE, value+1);
+                    //Ideally the date set here will be today's date
+                    contentValues.put(MacroContract.MacroEntry.COLUMN_INTAKE_DATE, "20171108");
+                    //The 'macro_name' in the where clause should match the quadrant and the update value
+                    //The date here should also ideally be today's date
+                    getContentResolver().update(MacroContract.MacroEntry.CONTENT_URI,contentValues, "macro_name = 'CARBOHYDRATES' AND intake_date = '20171108'", null);
+                }
+            });
+        } else if(id == 4) {
+            textView_quadrant_four.setText("Name:" + name + ", value:" + value);
+            button_quadrant_four.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Make an update call to DB
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(MacroContract.MacroEntry.COLUMN_MACRO_NAME, MACRO_SUGAR_DB_NAME);
+                    //just doing '+1' for now
+                    contentValues.put(MacroContract.MacroEntry.COLUMN_INTAKE_VALUE, value+1);
+                    //Ideally the date set here will be today's date
+                    contentValues.put(MacroContract.MacroEntry.COLUMN_INTAKE_DATE, "20171108");
+                    //The 'macro_name' in the where clause should match the quadrant and the update value
+                    //The date here should also ideally be today's date
+                    getContentResolver().update(MacroContract.MacroEntry.CONTENT_URI,contentValues, "macro_name = 'SUGAR' AND intake_date = '20171108'", null);
+                }
+            });
+        }
+
     }
 
     @Override
